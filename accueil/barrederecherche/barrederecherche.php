24467b7ff4bd078f9d5f7660192fbec64189ae49
sarrabah <sarrabenarbia1996@gmail.com>
2023-06-05 09:54:05 +0200

    improve search query

--- accueil/barrederecherche/barrederecherche.php
+++ accueil/barrederecherche/barrederecherche.php
@@ -12,37 +12,19 @@
         <input type="submit" value="valider" />
     </form>
 
-    <?php
-    
+    <?php   
     // include "../config.php";
-    include "../accueil/../connectdatabase.php";
-   
-    
+    include "../accueil/../connectdatabase.php";   
     if(isset($_GET['q']) AND !empty($_GET['q'])) {
         
         $q = $_GET['q'];
         
-        $nom= $conn->query('SELECT * From users WHERE nom LIKE "%'.$q.'%"' );
-        $prenom = $conn->query('SELECT * FROM users WHERE prenom LIKE "%'.$q.'%"');
-        $index = 0 ; 
-        while($a = $nom->fetch()) {?>
+        $result= $conn->query('SELECT * From users WHERE (nom LIKE "%'.$q.'%") OR (prenom LIKE "%'.$q.'%")');
+        while($a = $result->fetch()) {?>
             <ul>
-                <li> <?php echo $a["nom"] ;
-                $index++; ?></li>
+                <li> <?php echo $a["nom"] ; ?></li>
             </ul>               
-         <?php  } 
-         //echo $index;
-            if ($index == 0 ){
-             while($b = $prenom->fetch()) {?>
-                <ul>
-                    <li> <?php echo $b["prenom"] ;?> </li>
-                </ul>
-   <?php } 
-}
-    
-    } else{
-        echo "ok";
-    }
-    ?>
+         <?php  } ?>
+<?php } ?>
 </body>
 </html>
