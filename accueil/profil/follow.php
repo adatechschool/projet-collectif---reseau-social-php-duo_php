59cb579313e85a8c05e4817ed7f56b18a86501f4
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-06-05 13:05:10 +0200

    option follow me: bouton + lien + ajout dans la base follow + delete

---
+++ accueil/profil/follow.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+
+    include "../../connectdatabase.php";
+    $getFollowedId=intval($_GET['followedid']); //sécurise la variable
+
+    if($getFollowedId!= $_SESSION['id']){// evite de se suivre soit meme
+        $alreadyFollowed =$conn->prepare('SELECT * FROM follow WHERE idFollower = ? AND idFollowee=?');
+        $alreadyFollowed ->execute(array($_SESSION['id'],$getFollowedId));
+        $alreadyFollowed = $alreadyFollowed->rowCount();
+
+        if($alreadyFollowed==0){
+            $addFollow=$conn->prepare('INSERT INTO follow(idFollower,idFollowee) VALUES (?,?)');
+            $addFollow->execute(array($_SESSION['id'],$getFollowedId));
+        } elseif($alreadyFollowed ==1){
+            $deleteFollow=$conn->prepare('DELETE FROM follow WHERE idFollower = ? AND idFollowee=?');
+            $deleteFollow->execute(array($_SESSION['id'],$getFollowedId));
+        }
+    }
+    header('Location:'.$_SERVER['HTTP_REFERER']);
+
+?>
--- accueil/profil/profil_view.php
+++ accueil/profil/profil_view.php
@@ -21,7 +21,8 @@ if(isset($_GET[GET_ID])) {
         $utilisateurs = [
             NOM => $dataUser[NOM],
             PRENOM => $dataUser[PRENOM],
-            MAIL => $dataUser[MAIL]
+            MAIL => $dataUser[MAIL],
+            GET_ID =>$currentId 
         ];
     }
 }
@@ -29,7 +30,8 @@ else {
     $utilisateurs = [
         NOM => $_SESSION[NOM],
         PRENOM => $_SESSION[PRENOM],
-        MAIL => $_SESSION[MAIL]
+        MAIL => $_SESSION[MAIL],
+        GET_ID =>$_SESSION[GET_ID]
     ];
 }
 
@@ -41,8 +43,9 @@ else {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_profil.css">
+    <!-- <link rel="stylesheet" href="style_profil.css"> -->
     <link rel="stylesheet" href="../../style_navbar.css">
+    <link rel="stylesheet" href="style_view_profil.css">
     <title>Profil</title>
 </head>
 <body>
@@ -60,24 +63,24 @@ else {
 <div class="profil">
 
     <div class="photo">
-        <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <!-- <form action="upload.php" method="POST" enctype="multipart/form-data">
             <label for="file">Fichier</label>
             <input type="file" name="file">
             <button type="submit">Enregistrer</button>
-        </form>
+        </form> -->
 
         <?php 
-
-
-    $req = $conn->query('SELECT photo FROM photo');
-    while($data = $req->fetch()){
-        // var_dump($data);
-        echo "<img src='uploads/".$data['photo']."' width='300px' ><br>";
-    }
+            $req = $conn->query('SELECT photo FROM photo');
+            while($data = $req->fetch()){
+                // var_dump($data);
+            echo "<img src='uploads/".$data['photo']."' width='300px' ><br>";
+            }
         ?>
     </div>
 
-    <div class="info_connexion"> Info <br>
+    <div class="info_du_profil">  <br>
+    <titre>Informations de profil</titre>
+    <p>
         <?php 
         // include "../../connectdatabase.php";
         
@@ -85,33 +88,39 @@ else {
         echo "Nom : ".$utilisateurs["nom"].'</br>';
         echo "Adresse Mail : ".$utilisateurs["mail"].'</br>';
         ?>
-
-        <form action="modification.php" method="post">
+    </p>
+        <!-- <form action="modification.php" method="post">
             <button>
                 <img src="./modify_icon.png" alt="">
             </button>
-        </form>
+        </form> -->
     </div>
 </div>
 
+<div class="follow">
+<?php
+if(isset($_SESSION['id']) AND $_SESSION['id']!=GET_ID){ // permet d'afficher le bouton uniquement si on est connecté
+?>
+<button class="follow_me"><a href="./follow.php?followedid=<?php echo $currentId ;?>">Follow Me</a></button>
+<?php } ?>
+</div>
+
 <div class="Info_perso_ajout">
-    <form method="post" action="traitement.php">
+    <!-- <form method="post" action="traitement.php">
         <p>
             <label for="pseudo">Ton pseudo:</label>
             <input type="text" name="pseudo" id="pseudo" placeholder="Ex : luludu44" size="30" maxlength="10"/>
         </p>
-    </form>
+    </form> -->
 
-    <form method="post" action="traitement.php">
+    <!-- <form method="post" action="traitement.php">
         <p>
             <label for="biographie">Biographie</label><br/>
-            <textarea name="ameliorer" id="ameliorer" rows="10" cols="50">
-                Qui es-tu?
-            </textarea>
+            <textarea placeholder="Qui es tu?" name="ameliorer" id="ameliorer" rows="10" cols="50"></textarea>
         </p>
-    </form>
+    </form> -->
 
-    <form method="post" action="Niveau.php">
+    <!-- <form method="post" action="Niveau.php">
         <label for="level-select">Quel est ton niveau ?</label>
         <select name="niveau" id="">
             <option value="">Choisie ton niveau</option>
@@ -120,14 +129,14 @@ else {
             <option value="">Archi-FAN !</option>
         </select>
         <input type="submit" value="Valider">
-    </form>
+    </form> -->
 
-    <form method="post" action="traitement.php">
+    <!-- <form method="post" action="traitement.php">
         <p>
             <label for="pseudo">Ta ville:</label>
             <input type="text" name="pseudo" id="pseudo" placeholder="Ex : Paris" size="30" maxlength="10"/>
         </p>
-    </form>
+    </form> -->
 </div>
 
 </body>
