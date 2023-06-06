71828199b28b2a3270dceabacd505d55b485ed14
sarrabah <sarrabenarbia1996@gmail.com>
2023-06-06 11:57:06 +0200

    add comments without pseudo

--- accueil/post/post.php
+++ accueil/post/post.php
@@ -13,64 +13,71 @@
 
         if (isset($_POST['submit_commentaire'])){
 
-            if(isset($_POST['pseudo'],$_POST['commentaire']) AND !empty($_POST['pseudo']) AND !empty($_POST['commentaire'])){
-                $pseudo = $_POST["pseudo"];
+            if(isset($_POST['commentaire']) AND !empty($_POST['commentaire'])){
+                //$pseudo = $_POST["pseudo"];
                 $commentaire = $_POST["commentaire"];
-                if(strlen($pseudo) < 25){
-                    $ins ="INSERT INTO `commentaire` (`pseudo`, `commentaire`, `id_article`) VALUES ('$pseudo','$commentaire',1)";
+                //if(strlen($pseudo) < 25){
+                    $ins ="INSERT INTO `commentaire` (`commentaire`, `id_article`) VALUES ('$commentaire',1)";
                     $result = $conn->query($ins);
                     $c_msg="<span style='color: green'> Votre commentaire a bien été posté </span>";                    
-                }
-                else{
-                    $c_msg="Le pseudo doit faire moins de 25 caractéres";
-                }
+                //}
+                //else{
+                  //  $c_msg="Le pseudo doit faire moins de 25 caractéres";
+                //}
             }
             else{
                 $c_msg= "Erreur: Tous les champs doivent etre complétés";
             }
         }
         
-        $commentaires = "SELECT * FROM commentaire WHERE id_article =1 ORDER BY id Desc ";
+        $commentaires = "SELECT commentaire FROM commentaire WHERE id_article =1 ORDER BY id Desc ";
         $resultCom = $conn->query($commentaires);
     ?>
     <br />
+
     <div>    
         <?php
-        session_start();
-        $nom = $_SESSION["nom"]; 
-        $prenom = $_SESSION["prenom"];
-        $prenomCapitalized = ucfirst($prenom); // Met la première lettre en majuscule
-        $nomCapitalized = ucfirst($nom);
-        echo  "Utilisateur : ". $nomCapitalized. "  ".$prenomCapitalized; 
-         ?>
-    
+        $nomotheruser=  $conn->query('SELECT * From users WHERE id = 2');
+        while($a = $nomotheruser->fetch()) {
+            echo " Utilisateur : ".$a["nom"]." ".$a["prenom"];
+               }  
+         ?>  
     </div>
     <br />
+
     <div class="description"> 
         <?php
     $id= $conn->query('SELECT * From articles WHERE id = 1');
         while($a = $id->fetch()) {
-          echo "Description : ".$a["contenu"] ;
+          echo "Post : ".$a["contenu"] ;
              } ?>
     </div>
     <div>
         <h4> Commentaires : </h4>
-        <form method ="POST">
-            <input type ="text" name= "pseudo" placeholder = "Votre pseudo"> <br />
+        <form method ="POST">           
             <textarea name="commentaire" placeholder="Votre commentaire..."></textarea> <br />
             <input type = "submit" value="Poster mon commentaire" name="submit_commentaire" />
         </form>
+        
      <?php 
+     //msg d'erreur
         if(isset($c_msg)){
             echo $c_msg; 
         }
      ?>
      </div> 
      <br /> 
+
      <div>
         <?php
+            session_start();
+            $nom = $_SESSION["nom"]; 
+            $prenom = $_SESSION["prenom"];
+
             while($c= $resultCom->fetch()){ ?>
-            <b><?= $c['pseudo'] ; ?> : </b> <?= $c['commentaire']; ?> <br />
+            <b><?= $nom. " ". $prenom ;         
+             
+            ?> : </b> <?= $c['commentaire']; ?> <br />
             <?php } ?>
 
     </div> 
